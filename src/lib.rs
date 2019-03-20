ef916a52bd5f71d1acdd5965bdaf6c681868799f
Avery Wagar <ajmw.subs@gmail.com>
2019-03-19 22:58:15 -0700

    finished tutorial

---
+++ src/lib.rs
@@ -0,0 +1,276 @@
+mod utils;
+use web_sys;
+use js_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+/// Set default width and height of universe
+const SIZE: u32 = 64;
+const DEBUG: bool = false;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+use fixedbitset::FixedBitSet;
+
+cfg_if::cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+use web_sys::console;
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
+    }
+}
+
+/// Cell enum, used to easially count nearby cells
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    /// Dead cell
+    Dead = 0,
+    /// Alive cell
+    Alive = 1,
+}
+
+/// Universe struct we use a linear vector instead of a double one
+#[wasm_bindgen]
+pub struct Universe {
+    /// Width of a given universe
+    width: u32,
+    /// Height of a given universe
+    height: u32,
+    /// Linear vector of cells, each row ends at width * n, whereas n = row
+    cells: FixedBitSet,
+}
+
+/// Exported to JS
+#[wasm_bindgen]
+impl Universe {
+    /// Create a new universe with a default size (i.e. 64 x 64) and pattern
+    pub fn new() -> Universe {
+        utils::set_panic_hook();
+
+        let width = SIZE;
+        let height = SIZE;
+
+        // Set size
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            // Generate inital world
+            cells.set(i, i % 2 == 0 || i % 7 == 0);
+        }
+
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    /// Clear universe
+    pub fn clear(&mut self){
+        let size = (self.width * self.height) as usize;
+        self.cells = FixedBitSet::with_capacity(size);
+    }
+
+    /// Clears universe and creates a new random state
+    pub fn random(&mut self){
+        let size = (self.width * self.height) as usize;
+        self.cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            let random_boolean = js_sys::Math::random() >= 0.5;
+            self.cells.set(i, random_boolean);
+        }
+    }
+
+
+    /// Set a cell
+    pub fn set_cell(&mut self, row: u32, column: u32, alive: bool) {
+        let idx = self.get_index(row, column);
+        self.cells.set(idx, alive);
+
+    }
+
+    /// Toggle a cell
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
+        let cell = self.cells[idx];
+        self.cells.set(idx, !cell);
+    }
+
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.clear()
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.clear()
+    }
+
+    /// Returns the width of the universe
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    /// Returns the height of the universe
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    /// Returns a pointer to the cells
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
+
+    /// Get index in the linear array
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    /// Get count of neighbors next to given coordinates
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+
+        let north = if row == 0 {
+            self.height - 1
+        } else {
+            row - 1
+        };
+
+        let south = if row == self.height - 1 {
+            0
+        } else {
+            row + 1
+        };
+
+        let west = if column == 0 {
+            self.width - 1
+        } else {
+            column - 1
+        };
+
+        let east = if column == self.width - 1 {
+            0
+        } else {
+            column + 1
+        };
+
+        let nw = self.get_index(north, west);
+        count += self.cells[nw] as u8;
+
+        let n = self.get_index(north, column);
+        count += self.cells[n] as u8;
+
+        let ne = self.get_index(north, east);
+        count += self.cells[ne] as u8;
+
+        let w = self.get_index(row, west);
+        count += self.cells[w] as u8;
+
+        let e = self.get_index(row, east);
+        count += self.cells[e] as u8;
+
+        let sw = self.get_index(south, west);
+        count += self.cells[sw] as u8;
+
+        let s = self.get_index(south, column);
+        count += self.cells[s] as u8;
+
+        let se = self.get_index(south, east);
+        count += self.cells[se] as u8;
+
+        count
+    }
+
+    /// Run a tick of the simulation
+    pub fn tick(&mut self) {
+        if DEBUG {
+            let _timer = Timer::new("Universe::tick");
+        }
+        let mut next = self.cells.clone();
+
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                // This KILLS preformance
+                // log!( "cell[{}, {}] is initially {:?} and has {} live neighbors", row, col, cell, live_neighbors);
+
+                next.set(idx, match (cell, live_neighbors) {
+                    (true, x) if x < 2 => false,
+                    (true, 2) | (true, 3) => true,
+                    (true, x) if x > 3 => false,
+                    (false, 3) => true,
+                    (otherwise, _) => otherwise
+                });
+            }
+        }
+
+        self.cells = next;
+    }
+
+}
+
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[u32] {
+        &self.cells.as_slice()
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
+}
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(s: &str) {
+    alert(&format!("Hello, {}!",s));
+}
+
---
+++ ww/index.js
@@ -0,0 +1,325 @@
+/*
+ * Logistics
+ */
+
+// Import the WebAssembly memory at the top of the file.
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+import { Universe } from "wasm-game-of-life";
+
+const CELL_SIZE = 10; // px
+const GRID_COLOR = "#333333";
+const DEAD_COLOR = "#000000";
+const ALIVE_COLOR = "#20C20E";
+
+// Construct the universe, and get its width and height.
+const universe = Universe.new();
+
+let width = universe.width();
+let height = universe.height();
+
+let animationId = null;
+
+const isPaused = () => {
+  return animationId === null;
+};
+
+const fps = new class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = 1 / delta * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+  }
+};
+
+/* 
+ * Input setup
+ */
+
+// Generation counter
+const genCounter = document.getElementById("gen");
+genCounter.textContent = 0;
+
+// Size
+const sizeInput = document.getElementById("size");
+sizeInput.value = 1;
+
+sizeInput.addEventListener("input", event => {
+  universe.clear();
+
+  let newsize = sizeInput.value / 5 + 64;
+
+
+  // Update canvas
+  width = newsize;
+  height = newsize;
+
+
+  // setup canvas
+  canvas.height = (CELL_SIZE + 1) * height + 1;
+  canvas.width = (CELL_SIZE + 1) * width + 1;
+
+  // Update univsers
+  universe.set_width(newsize);
+  universe.set_height(newsize);
+
+  play();
+  genCounter.value = 0;
+  pause();
+})
+
+
+// Speed slider
+const speedRange = document.getElementById("speed");
+speedRange.value = 1;
+
+// Play / Pause button
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+  playPauseButton.textContent = "⏸";
+  renderLoop();
+};
+
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animationId);
+  animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
+});
+
+// Clear button
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", event => {
+  pause();
+  universe.clear();
+  play();
+  pause();
+  genCounter.textContent = 0;
+});
+
+// Random button
+const randomButton = document.getElementById("random");
+randomButton.addEventListener("click", event => {
+  pause();
+  universe.random();
+  play();
+  genCounter.textContent = 0;
+
+});
+
+/* 
+ * Rendering for canvas
+ */
+
+// Grab canvas to render on
+let canvas = document.getElementById("game-of-life-canvas");
+
+// setup canvas
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+// TODO Implement WebGL
+const ctx = canvas.getContext('2d');
+
+canvas.addEventListener("click", event => {
+  const boundingRect = canvas.getBoundingClientRect();
+
+  const scaleX = canvas.width / boundingRect.width;
+  const scaleY = canvas.height / boundingRect.height;
+
+  const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+  const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+  if (event.shiftKey) {
+    // Create a glider
+    console.log("Glider")
+    universe.set_cell(row, col, true);
+    universe.set_cell(row, col, false);
+    universe.set_cell(row + 1, col, true);
+    universe.set_cell(row - 1, col, true);
+    universe.set_cell(row, col + 1, true);
+    universe.set_cell(row - 1, col - 1, true);
+    universe.set_cell(row - 1, col + 1, true);
+
+  }
+  else if (event.ctrlKey) {
+    // Create a pulsar
+    console.log("Pulsar")
+    // TODO Implement pulsar
+  }
+  else {
+    // set cell
+    console.log("toggle")
+    universe.toggle_cell(row, col);
+  }
+
+  drawGrid();
+  drawCells();
+});
+
+/*
+ * Game code
+ */
+
+// Create grid 
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+
+const bitIsSet = (n, arr) => {
+  const byte = Math.floor(n / 8);
+  const mask = 1 << (n % 8);
+  return (arr[byte] & mask) === mask;
+};
+
+// Draw cells on canvas
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+
+  // This is updated!
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height / 8);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      // This is updated!
+      ctx.fillStyle = bitIsSet(idx, cells)
+        ? ALIVE_COLOR
+        : DEAD_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  // Alive cells.
+  ctx.fillStyle = ALIVE_COLOR;
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+      if (!bitIsSet(idx, cells)) {
+        continue;
+      }
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  // Dead cells.
+  ctx.fillStyle = DEAD_COLOR;
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+      if (bitIsSet(idx,cells)) {
+        continue;
+      }
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+  ctx.stroke();
+};
+
+
+// Render loop
+const renderLoop = () => {
+  fps.render();
+
+  // console.log(speedRange.value / 10);
+  for (let i = 0; i <= speedRange.value / 10; i++) {
+    genCounter.textContent++;
+    // console.log("ticking")
+    universe.tick();
+  }
+  drawGrid();
+  drawCells();
+  animationId = requestAnimationFrame(renderLoop);
+};
+
+
+// Render initial unverse
+play();
+
